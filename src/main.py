69ab89b0478977f3f7a84b29353e6f31503a34e0
Francisco-Parente <franciscoparente96@gmail.com>
2024-11-13 17:18:35 +0100

    added maze class

--- src/main.py
+++ src/main.py
@@ -1,30 +1,10 @@
 from window import Window
-from point import Point, Line, Cell
+from maze import Point, Line, Cell, Maze
 
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(Point(100, 100), Point(150, 150), win)
-    cell1.draw()
-
-    cell2 = Cell(Point(200, 200), Point(250, 250), win)
-    cell2.has_left_wall = False
-    cell2.draw()
-
-    cell3 = Cell(Point(300, 300), Point(350, 350), win)
-    cell3.has_right_wall = False
-    cell3.draw()
-
-    cell4 = Cell(Point(400, 400), Point(450, 450), win)
-    cell4.has_top_wall = False
-    cell4.draw()
-
-    cell5 = Cell(Point(500, 500), Point(550, 550), win)
-    cell5.has_bottom_wall = False
-    cell5.draw()
-
-    cell1.draw_move(cell2)
-    cell2.draw_move(cell3, undo = True)
+    maze = Maze(100, 100, 5, 5, 50, 50, win)
 
     win.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,5 @@
+import time
+
 from tkinter import Canvas
 
 class Point():
@@ -46,5 +48,51 @@ class Cell():
         else:
             self._win.draw_line(Line(self.get_center(), to_cell.get_center()), "red")
 
-    def get_center(self):
+    def _get_center(self):
         return Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
+
+class Maze():
+
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win
+        ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_cols):
+            collumn = []
+            for j in range(self.num_rows):
+                cell = Cell(
+                    Point(self.x1 + j * self.cell_size_x, self.y1 + i * self.cell_size_y), 
+                    Point(self.x1 + (j + 1) * self.cell_size_x, self.y1 + (i + 1) * self.cell_size_y),
+                    self.win
+                )
+                collumn.append(cell)
+            self._cells.append(collumn)
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        self._cells[i][j].draw()
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+

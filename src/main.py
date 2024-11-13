fb6695dcb8b89c569025f984bf28281859b8e7ab
Francisco-Parente <franciscoparente96@gmail.com>
2024-11-13 16:02:23 +0100

    added point.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from window import Window
+from point import Point, Line
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(100, 100), Point(100, 200))
+    win.draw_line(line, "black")
+    line2 = Line(Point(200, 200), Point(300, 200))
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
